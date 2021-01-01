1975965849bbd99a0649233cd053a8a6e1b5a0fc
sabsabila <58177825+sabsabila@users.noreply.github.com>
2021-01-01 12:10:06 +0700

    add splash screen

--- app/src/main/java/pens/lab/app/belajaractivity/modul/todo/ToDoActivity.java
+++ app/src/main/java/pens/lab/app/belajaractivity/modul/todo/ToDoActivity.java
@@ -1,16 +1,8 @@
 package pens.lab.app.belajaractivity.modul.todo;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
-
-import java.util.ArrayList;
-
 import pens.lab.app.belajaractivity.R;
 import pens.lab.app.belajaractivity.base.BaseFragmentHolderActivity;
-import pens.lab.app.belajaractivity.model.Task;
-
 
 public class ToDoActivity extends BaseFragmentHolderActivity {
     @Override
